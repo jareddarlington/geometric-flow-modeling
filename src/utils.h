7c0d84d615e2b8603da1d0c07525635aebb4ce4b
Jared Darlington <darlingtonjwd@gmail.com>
2024-07-21 12:22:16 -0700

    removed dynamic array implementation

--- src/utils.h
+++ src/utils.h
@@ -7,42 +7,8 @@
 #include <stdlib.h>
 
 /*
- * Structures
- */
-
-typedef struct
-{
-    float *array; // array
-    size_t size;  // size
-    size_t cap;   // capacity
-} DynamicArray;
-
-/*
  * Function Prototypes
  */
-
-/**
- * @brief Initializes a dynamic array.
- *
- * @param dynArray Dynamic array to initialize.
- * @param initCap  Initial capacity of dynamic array.
- */
-void init(DynamicArray *dynArray, size_t initCap);
-
-/**
- * @brief Pushes value to dynamic array.
- *
- * @param dynArray Dynamic array to push to.
- * @param value    Value to push.
- */
-void push(DynamicArray *dynArray, float value);
-
-/**
- * @brief Clears dynamic array.
- *
- * @param dynArray Dynamic array to clear.
- */
-void clear(DynamicArray *dynArray);
 
 /**
  * @brief Custom getline as Windows doesn't follow POSIX standards. Credit goes to Will Hartung and Antti Haapala.
